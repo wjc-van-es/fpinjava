303abeed372e5c9e2e958dca02f45d887a796e04
Willem van Es <wjc.van.es@gmail.com>
2024-08-02 00:24:04 +0200

    § 2.2 exercises

--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/test/java/com/fpinjava/functions/exercise02_01/FunctionExamplesTest.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/test/java/com/fpinjava/functions/exercise02_01/FunctionExamplesTest.java
@@ -9,11 +9,11 @@ import org.junit.jupiter.api.Test;;
 
 public class FunctionExamplesTest {
 
-  @Test
-  public void testCompose() {
-    assertEquals(Integer.valueOf(6), triple.apply(2));
-    assertEquals(Integer.valueOf(4), square.apply(2));
-    assertEquals(Integer.valueOf(36), square.apply(triple.apply(2)));
-    assertEquals(Integer.valueOf(27), compose(triple, square).apply(3));
-  }
+    @Test
+    public void testCompose() {
+        assertEquals(Integer.valueOf(6), triple.apply(2));
+        assertEquals(Integer.valueOf(4), square.apply(2));
+        assertEquals(Integer.valueOf(36), square.apply(triple.apply(2)));
+        assertEquals(Integer.valueOf(27), compose(triple, square).apply(3));
+    }
 }
